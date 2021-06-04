f297ba1131bfb00f7eb39f52d4cafb986776744b
Matheus Santana <matheussantanaengineer@gmail.com>
2021-06-03 21:32:11 -0300

    Initial commit

---
+++ src/config/config.js
@@ -0,0 +1,14 @@
+// Define a string de conexão com o banco de dados
+//comentario
+require('dotenv/config');
+ 
+module.exports = {
+    database: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        name: process.env.DB_NAME,
+        dialect: process.env.DB_DIALECT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD
+    }
+}
---
+++ src/controllers/produtoController.js
@@ -0,0 +1,108 @@
+// Define a utilização do model produto e a dependência http-status
+const Produto = require('../models/produto');
+const status = require('http-status');
+ 
+// Cria o método Insert, obtendo os dados da request
+exports.Insert = (req, res, next) => {
+    const nome = req.body.nome;
+    const descricao = req.body.descricao;
+    const preco = req.body.preco;
+    const estoque = req.body.estoque;
+ 
+    // Popula cada um dos campos do model com os campos recebido na request
+    Produto.create({
+        nome: nome,
+        descricao: descricao,
+        preco: preco,
+        estoque: estoque,
+    })
+        //then = registra o que queremos que aconteca quando a Promise for resolvida
+        .then(produto => {
+            if (produto) {
+                res.status(status.OK).send(produto);
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        //catch = registra o que queremos que aconteca quando a Promise falhar
+        .catch(error => next(error));
+};
+ 
+//CRIA O METODO SELECT ALL
+exports.SelectAll = (req, res, next) => {
+    Produto.findAll()
+        .then(produto => {
+            if (produto) {
+                res.status(status.OK).send(produto);
+            }
+        })
+        .catch(error => next(error));
+}
+ //CRIA O METODO SELECT DETAIL
+exports.SelectDetail = (req, res, next) => {
+    const id = req.params.id;
+ 
+    Produto.findByPk(id)
+        .then(produto => {
+            if (produto) {
+                res.status(status.OK).send(produto);
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+  //CRIA O METODO UPDATE
+
+exports.Update = (req, res, next) => {
+    const id = req.params.id;
+    const nome = req.body.nome;
+    const descricao = req.body.descricao;
+    const preco = req.body.preco;
+    const estoque = req.body.estoque;
+ 
+    Produto.findByPk(id)
+        .then(produto => {
+            if (produto) {
+                produto.update({
+                    nome: nome,
+                    descricao: descricao,
+                    preco: preco,
+                    estoque: estoque
+                },
+                    {
+                        where: { id: id }
+                    })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+  //CRIA O METODO DELETE
+
+exports.Delete = (req, res, next) => {
+    const id = req.params.id;
+ 
+    Produto.findByPk(id)
+        .then(produto => {
+            if (produto) {
+                produto.destroy({
+                    where: { id: id }
+                })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            }
+            else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+//comentario
